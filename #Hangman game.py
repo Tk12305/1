f59cff71d72617b7d19ce9508b0007b1086d3fd0
tbgglking <tbgglking@gmail.com>
2025-03-27 20:14:08 +0000

    update from google idx

--- #Hangman game.py
+++ #Hangman game.py
@@ -1,13 +1,18 @@
 import random
 
+win = False
+
 def making_a_guess():
+    global win
     X = 0
-    global update_display
+    global update_display, win
     correct_guess = False
     for letter in chosen_word:
         if guess.lower() == chosen_word[X]:
             blank[X] = guess.lower()
             correct_guess = True
+            if list(blank) == chosen_word:
+                win = True
         X += 1
     if not correct_guess:
         print("There is no {guess}, sorry.")
@@ -72,14 +77,12 @@ chosen_word = list(random.choice(WORD_LIST))
 blank = "_" * len(chosen_word)
 
 update_display = 0
- 
-
 
 print(HANGMANPICS[update_display])
 guess = input("Welcome to hangman Make a guess? ")
 making_a_guess()
 print(HANGMANPICS[update_display])
-print(''.join(blank))
+print(''.join(list(blank)))
 while update_display < 6:
     if blank == chosen_word:
         print("YOU WIN!")
@@ -88,6 +91,6 @@ while update_display < 6:
     guess = input("Make another guess? ")
     making_a_guess()
     print(HANGMANPICS[update_display])
-    print(''.join(blank))
+    print(''.join(list(blank)))
 if update_display == 6:
     print("GAME OVER.") 
